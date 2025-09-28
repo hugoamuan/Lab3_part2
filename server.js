17557318c6c662ceb49e511965c823bc404c34d8
hugoamuan <hugo.amuan@icloud.com>
2025-09-27 22:06:34 -0700

    /writeFile/ fix

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const userMessages = require('./lang/en/en.js');
 
 const outputFolder = "output";
 
+// Make sure /output/ directory exists
 if (!fs.existsSync(outputFolder)) {
     fs.mkdirSync(outputFolder);
 }
 
-// Digital Ocean provides port by default
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // DigitalOcean provides PORT
 
 http.createServer((req, res) => {
     console.log("Server received a request.");
@@ -19,7 +19,7 @@ http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const pathname = parsedUrl.pathname;
 
-    // handle /getDate/ request
+    // /getDate/ path
     if (pathname.endsWith('getDate/')) {
         const current_username = parsedUrl.query.name || "Guest";
         const currentDate = getDate();
@@ -31,11 +31,9 @@ http.createServer((req, res) => {
         });
         res.end(message);
 
-    // handle /writeFile/ requests
-    } else if (pathname.startsWith('/writeFile/')) {
-        const parts = pathname.split('/'); // ["", "writeFile", "file.txt"]
-        const fileName = parts[2];
-        const filePath = `${outputFolder}/${fileName}`;
+    // handle /writeFile/ url path
+    } else if (pathname.endsWith('writeFile/')) {
+        const filePath = `${outputFolder}/file.txt`; 
         const text = parsedUrl.query.text || "";
         const logLine = `${text}\n`;
 
@@ -48,10 +46,10 @@ http.createServer((req, res) => {
             }
 
             res.writeHead(200, { "Content-Type": "text/plain" });
-            res.end(`Appended text to ${fileName}: ${text}`);
+            res.end(`Appended text to file.txt: ${text}`);
         });
 
-    // handle /readFile/ requests
+    // handle /readFile/ url path
     } else if (pathname.startsWith('/readFile/')) {
         const parts = pathname.split('/'); // ["", "readFile", "file.txt"]
         const fileName = parts[2];
@@ -69,10 +67,10 @@ http.createServer((req, res) => {
                 "Content-Type": "text/html",
                 "Access-Control-Allow-Origin": "*"
             });
-            res.end(`<pre>${textData}</pre>`); 
+            res.end(`<pre>${textData}</pre>`); // <pre>
         });
 
-    // Invalid path
+    // --- Unknown path ---
     } else {
         res.writeHead(404, { "Content-Type": "text/plain" });
         res.end("404 Not Found");
